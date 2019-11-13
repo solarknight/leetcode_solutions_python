0c6e840211d99eafb953a01d4655ec0db34feb9d
solarknight <zhouthanos@gmail.com>
2019-11-14 00:04:53 +0800

    Update 3sum_closest.py

--- 16_3sum_closest.py
+++ 16_3sum_closest.py
@@ -1,9 +1,24 @@
+import sys
 from typing import List
 
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        pass
+        s_nums, sum, cur_shift, min_shift = sorted(nums), 0, 0, sys.maxsize
+        for i in range(len(s_nums) - 2):
+            left, right = i + 1, len(s_nums) - 1
+            while left < right:
+                cur_shift = s_nums[left] + s_nums[right] + s_nums[i] - target
+                if abs(cur_shift) < min_shift:
+                    min_shift = abs(cur_shift)
+                    sum = cur_shift + target
+                if cur_shift == 0:
+                    return target
+                elif cur_shift < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return sum
 
 
 if __name__ == '__main__':
